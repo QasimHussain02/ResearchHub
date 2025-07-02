f0164b1b184eeddb9d0197cedf7874af44d46450
qasim <qhofficial12345@gmail.com>
2025-07-02 14:43:10 +0500

    added comment

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import SignupComp from "../components/signupComp";
 
 export default function Signup() {
+  // This component renders the SignupComp component
   return <SignupComp />;
 }
